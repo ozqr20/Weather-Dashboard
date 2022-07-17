ff338d8775277dc3a151658e1d50a55953063c0b
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-07-17 19:01:51 -0400

    querySelectors

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var cities = []
+var InputSearchEl = document.querySelector("#input-search");
+var citySearchEl = document.querySelector("#city");
+var historySerchEl = document.querySelector("#history-search-btns");
+var currentWeatherEl = document.querySelector("#current-weather");
+var searchedCityEl = document.querySelector("#search-city");
+var weatherContainerEl = document.querySelector("#weather-container");
+var fiveDaysEl = document.querySelector("#five-days");
+var forecastEl = document.querySelector("#forecast");
+var displayFiveEl = document.querySelector("#display-fivedays")
+
+
