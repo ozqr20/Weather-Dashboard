c535d648159d1d98a6721f802f1c3b3fee91856c
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-07-17 22:35:21 -0400

    apiURL & saveSearch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var cities = []
 var InputSearchEl = document.querySelector("#input-search");
-var citySearchEl = document.querySelector("#city");
+var cityEl = document.querySelector("#city");
 var historySerchEl = document.querySelector("#history-search-btns");
 var currentWeatherEl = document.querySelector("#current-weather");
 var searchedCityEl = document.querySelector("#search-city");
@@ -9,4 +9,22 @@ var fiveDaysEl = document.querySelector("#five-days");
 var forecastEl = document.querySelector("#forecast");
 var displayFiveEl = document.querySelector("#display-fivedays")
 
+var saveSearch = function(){
+    localStorage.setItem("cities", JSON.stringify(cities));
+}
 
+var cityWeather = function(city){
+    const apiKey = "1767dcfe210ef96ad104c047ad61f1bb";
+    var apiUrl =  "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=1767dcfe210ef96ad104c047ad61f1bb"
+
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+            getWeather(data,city)
+        });
+    });
+};
+
+var getWeather = function(weather, city){
+    currentWeatherEl.textContent = "";
+    InputSearchEl.textContent = city;
+}
