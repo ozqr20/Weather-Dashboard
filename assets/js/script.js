33650cb9356d212a2af623aa4a346e503c3b9b48
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-07-19 22:39:05 -0400

    uv display functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,9 @@ var weatherContainerEl = document.querySelector("#weather-container-now");
 var fiveDaysEl = document.querySelector("#five-days");
 var forecastEl = document.querySelector("#forecast");
 var displayFiveEl = document.querySelector("#display-fivedays")
+const apiKey = "1767dcfe210ef96ad104c047ad61f1bb";
+
+
 
 var saveSearch = function(){
     localStorage.setItem("cities", JSON.stringify(cities));
@@ -15,7 +18,6 @@ var saveSearch = function(){
 
 
 var cityWeather = function(city){
-    var apiKey = "1767dcfe210ef96ad104c047ad61f1bb";
     var apiUrl =  ("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey);
 
     fetch(apiUrl).then(function(response){
@@ -39,7 +41,7 @@ var getWeather = function(weather, city){
     searchedCityEl.textContent = city;
 
     var date = document.createElement("span");
-    date.textContent = "(" + moment(weather.dt.value).add(1, "days").calendar() + ")";
+    date.textContent = "(" + moment(weather.dt.value).add(0.1, "days").calendar() + ")";
     searchedCityEl.appendChild(date);
 
     var icon = document.createElement("img");
@@ -54,7 +56,6 @@ var getWeather = function(weather, city){
     var temp = document.createElement("span");
     temp.textContent = "Temperature: " + weather.main.temp + "ºF";
     temp.classList = "list-group-item";
-    //temp = Math.round(temp((-273.15*1.8)+32));
     searchedCityEl.appendChild(temp);
 
     var humidity = document.createElement("span");
@@ -64,8 +65,44 @@ var getWeather = function(weather, city){
 
     var lon = weather.coord.lon;
     var lat = weather.coord.lat;
-    //getUv(lon,lat)
+    getUv(lon,lat)
 } 
+
+var getUv = function (lon,lat){
+    var apiUrl = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
+    fetch(apiUrl).then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                displayUv(data);
+            });
+        }else{
+            alert("Error");
+        };   
+    })
+    .catch(function(error){
+        alert("Unable to connect");
+    });
+};
+
+var displayUv = function (data){
+    var uvIndex = document.createElement("div");
+    uvIndex.textContent = "UV Index: ";
+    uvIndex.classList = "list-group-item"
+
+    uvValue = document.createElement("span");
+    uvValue.textContent = data.value;
+
+    if(data.value > 8){
+        uvValue.classList = "severe alert alert-danger";
+    } else if (data.value > 2){
+        uvValue.classList = "moderate alert alert-secondary"
+    } else{
+        uvValue.classList = "favorable alert alert-primary";
+    };
+
+    uvIndex.appendChild(uvValue);
+    searchedCityEl.appendChild(uvIndex);
+}
 
 var formSubmitHandler = function(event){
     event.preventDefault();
@@ -79,4 +116,3 @@ var formSubmitHandler = function(event){
 } 
 
 InputSearchEl.addEventListener("submit",formSubmitHandler);
-//cityWeather("Chicago")
